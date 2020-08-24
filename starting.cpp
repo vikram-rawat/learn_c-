65e8506e9b93b992715be11483e30a331fbda95d
Anantadinath <vickey.coool@gmail.com>
2020-08-24 20:55:46 +0530

    push changes

--- starting.cpp
+++ starting.cpp
@@ -27,7 +27,6 @@ void Timeseries::display()
 {
     cout << " TsValue is " << TsValue << endl;
     cout << " TsDate is " << TsDate << endl;
-    //
     // Don't use PrintF it is not typesafe and will have issues for custom types
     // printf("%s is TsDate \n and %d is TsValue", TsDate.c_str(), TsValue);
 }
